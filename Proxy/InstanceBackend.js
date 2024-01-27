71c06496d91be30a03eb0545894f49ace9810afa
muneer <Muneer@007>
2024-01-27 21:15:15 +0530

    Proxy is added

--- Proxy/InstanceBackend.js
+++ Proxy/InstanceBackend.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const instanceBackend=axios.create({
-    baseURL:'https://assignment-3-woad.vercel.app/',
-    withCredentials:true
+    baseURL:'https://assignment-3-sooty.vercel.app/',
 })
 
 export default instanceBackend;
